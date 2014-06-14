e42f97dac42b08fbbc765395e7a4072932aa40a5
Dominik Zogg <dominik.zogg@gmail.com>
2014-06-14 13:00:51 +0200

    fix checkbox layout

--- src/Dominikzogg/EnergyCalculator/Form/UserType.php
+++ src/Dominikzogg/EnergyCalculator/Form/UserType.php
@@ -3,10 +3,22 @@
 namespace Dominikzogg\EnergyCalculator\Form;
 
 use Saxulum\UserProvider\Form\AbstractUserType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class UserType extends AbstractUserType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->remove('enabled');
+        $builder->add('enabled', 'checkbox', array(
+            'required' => false,
+            'attr' => array(
+                'align_with_widget' => true
+            )
+        ));
+    }
+
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         parent::setDefaultOptions($resolver);
