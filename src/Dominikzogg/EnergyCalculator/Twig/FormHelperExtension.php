7ffa8f465fb3c3dd81d6449fe575beabc2b66cbe
Dominik Zogg <dominik.zogg@gmail.com>
2015-01-21 20:37:53 +0100

    enhance form label creation

--- src/Dominikzogg/EnergyCalculator/Twig/FormHelperExtension.php
+++ src/Dominikzogg/EnergyCalculator/Twig/FormHelperExtension.php
@@ -9,30 +9,35 @@ class FormHelperExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            new \Twig_SimpleFunction('prepareFormLabel', function(FormView $formView) {
-                $labelParts = array();
-                $collection = false;
-                do {
-                    $name = $formView->vars['name'];
-                    if(is_numeric($name) || $name === '__name__') {
-                        $collection = true;
-                    } else {
-                        if($collection) {
-                            $name .= '_collection';
-                        }
-                        $labelParts[] = $name;
-                        $collection = false;
-                    }
-
-                } while($formView = $formView->parent);
-
-                $length = count($labelParts);
-                $labelParts[$length] = $labelParts[$length-1];
-                $labelParts[$length-1] = 'label';
-
-                return implode('.', array_reverse($labelParts));
-            })
+            new \Twig_SimpleFunction('prepareFormLabel', array($this, 'prepareFormLabel')),
         );
+    }
+
+    /**
+     * @param FormView $formView
+     * @return string
+     */
+    public function prepareFormLabel(FormView $formView)
+    {
+        $labelParts = array();
+        $collection = false;
+        do {
+            $name = $formView->vars['name'];
+            if(is_numeric($name) || $name === '__name__') {
+                $collection = true;
+            } else {
+                if($collection) {
+                    $labelParts[] = $name . '_collection';
+                } else {
+                    $labelParts[] = str_replace('_', '.', $name);
+                }
+                $collection = false;
+            }
+        } while($formView = $formView->parent);
+        $length = count($labelParts);
+        $labelParts[$length] = $labelParts[$length-1];
+        $labelParts[$length-1] = 'label';
+        return implode('.', array_reverse($labelParts));
     }
 
     /**
