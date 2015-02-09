78bb910ac87e08f31f13a0770afaecbbf6e5a2e5
Dominik Zogg <dominik.zogg@gmail.com>
2015-02-09 20:52:39 +0100

    simplify code

--- src/Dominikzogg/EnergyCalculator/Form/SimpleDateType.php
+++ src/Dominikzogg/EnergyCalculator/Form/SimpleDateType.php
@@ -2,43 +2,10 @@
 
 namespace Dominikzogg\EnergyCalculator\Form;
 
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormError;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class SimpleDateType extends AbstractType
 {
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array                $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        parent::buildForm($builder, $options);
-
-        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
-            $data = $formData = $event->getData();
-            if (is_string($data)) {
-                if ($data) {
-                    try {
-                        $data = $this->getData(new \DateTime($data));
-                    } catch (\Exception $e) {
-                        $event->getForm()->addError(
-                            new FormError('This value is not a valid date.', null, array(
-                                '%dateformat%' => $this->getDateFormat(),
-                            ))
-                        );
-                    }
-                } else {
-                    $data = $this->getData();
-                }
-                $event->setData($data);
-            }
-        });
-    }
-
     /**
      * @param OptionsResolverInterface $resolver
      */
@@ -53,23 +20,6 @@ class SimpleDateType extends AbstractType
                 'data-date-format' => 'dd.mm.yyyy',
             ),
         ));
-    }
-
-    /**
-     * @param  \DateTime $dateTime
-     * @return string
-     */
-    protected function getData(\DateTime $dateTime = null)
-    {
-        return null !== $dateTime ? $dateTime->format($this->getDateFormat()) : '';
-    }
-
-    /**
-     * @return string
-     */
-    protected function getDateFormat()
-    {
-        return 'd.m.Y';
     }
 
     /**
